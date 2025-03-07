e6019aa233411509a20ba16905a57c70f4ed3024
Oskkyyy <yaiv.gm@gmail.com>
2025-03-06 21:18:09 -0600

    Se actualizo app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
+import * as React from 'react';
+import FilledInput from '@mui/material/FilledInput';
+import FormControl from '@mui/material/FormControl';
+import FormHelperText from '@mui/material/FormHelperText';
+import Input from '@mui/material/Input';
+import InputLabel from '@mui/material/InputLabel';
+import OutlinedInput from '@mui/material/OutlinedInput';
 import './App.css';
+import {Box, Container, Link, Typography} from "@mui/material";
 
-function App() {
+function Copyright() {
+    return (
+        <Typography sx={{mb: 2}}>
+            { 'Copyright ' }
+            <Link href="https://github.com/Oskkyyy">
+                {' Git '}
+            </Link>
+            { new Date().getFullYear() }
+        </Typography>);
+}
+
+function ComposedTextField() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box
+      component="form"
+      sx={{ '& > :not(style)': { m: 1 } }}
+      noValidate
+      autoComplete="off"
+    >
+      <FormControl variant="standard">
+        <InputLabel htmlFor="component-simple">Name</InputLabel>
+        <Input id="component-simple" defaultValue="Composed TextField" />
+      </FormControl>
+      <FormControl variant="standard">
+        <InputLabel htmlFor="component-helper">Name</InputLabel>
+        <Input
+          id="component-helper"
+          defaultValue="Composed TextField"
+          aria-describedby="component-helper-text"
+        />
+        <FormHelperText id="component-helper-text">
+          Some important helper text
+        </FormHelperText>
+      </FormControl>
+      <FormControl disabled variant="standard">
+        <InputLabel htmlFor="component-disabled">Name</InputLabel>
+        <Input id="component-disabled" defaultValue="Composed TextField" />
+        <FormHelperText>Disabled</FormHelperText>
+      </FormControl>
+      <FormControl error variant="standard">
+        <InputLabel htmlFor="component-error">Name</InputLabel>
+        <Input
+          id="component-error"
+          defaultValue="Composed TextField"
+          aria-describedby="component-error-text"
+        />
+        <FormHelperText id="component-error-text">Error</FormHelperText>
+      </FormControl>
+      <FormControl>
+        <InputLabel htmlFor="component-outlined">Name</InputLabel>
+        <OutlinedInput
+          id="component-outlined"
+          defaultValue="Composed TextField"
+          label="Name"
+        />
+      </FormControl>
+      <FormControl variant="filled">
+        <InputLabel htmlFor="component-filled">Name</InputLabel>
+        <FilledInput id="component-filled" defaultValue="Composed TextField" />
+      </FormControl>
+    </Box>
   );
 }
 
-export default App;
+export default function MyApp() {
+    return (
+        <Container maxWidth="sm">
+            <Box sx={{my: 4}}>
+                <Typography variant="h4" component="h1" sx={{mb: 2}}>
+                    Mi primera App con React y Material UI
+                </Typography>
+                <ComposedTextField/>
+                <Copyright/>
+            </Box>
+        </Container>
+    );
+}
